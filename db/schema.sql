2375b6f3a7f20a97d72f3d4b7265ba2ad79382bc
kwaters3 <knickler3@gmail.com>
2023-10-01 22:06:37 -0400

    updated app to properly function

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR (255) NOT NULL,
   salary DECIMAL (10, 2) NOT NULL, 
-  department_id INT NOT NULL,
+  department_id INT,
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES department (id)
   ON DELETE SET NULL
